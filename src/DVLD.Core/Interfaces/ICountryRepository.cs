c88646e349972498d6a942d4e86b64fc183a31d8
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-10 12:23:47 +0300

    Add ICountryRepository interface and update project structure
    Removed folder entries from DVLD.Core.csproj to simplify the
    project structure. Added the ICountryRepository interface to
    the DVLD.Core.Interfaces namespace, defining methods for
    retrieving Country entities by ID, name, or as a collection.
    Included necessary using directives to support the new interface.

---
+++ src/DVLD.Core/Interfaces/ICountryRepository.cs
@@ -0,0 +1,16 @@
+﻿using DVLD.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Core.Interfaces
+{
+    public interface ICountryRepository
+    {
+        Task<Country?> GetByIDAsync(int countryID);
+        Task<Country?> GetByNameAsync(string countryName);
+        Task<IEnumerable<Country>> GetAllAsync();
+    }
+}
