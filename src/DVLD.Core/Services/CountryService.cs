30369c9aa7ce8673210ca07b7534267c888c2abd
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-10 12:47:07 +0300

    Add CountryService with in-memory country data
    Introduced a new `CountryService` class implementing the `ICountryService` interface. Added a temporary in-memory list of countries to simulate a data source. Implemented methods to retrieve all countries, a country by ID, and a country by name asynchronously. Included necessary `using` directives for DTOs and interfaces.

---
+++ src/DVLD.Core/Services/CountryService.cs
@@ -0,0 +1,32 @@
+﻿using DVLD.Core.DTOs;
+using DVLD.Core.Interfaces;
+
+namespace DVLD.Core.Services;
+
+public class CountryService : ICountryService
+{
+    // قائمة تجريبية مؤقتة داخل الخدمة لعزل الكنترولر تماماً
+    private static readonly List<CountryDTO> _countries = new()
+    {
+        new CountryDTO { CountryID = 1, CountryName = "Yemen" },
+        new CountryDTO { CountryID = 2, CountryName = "Saudi Arabia" },
+        new CountryDTO { CountryID = 3, CountryName = "Egypt" }
+    };
+
+    public Task<IEnumerable<CountryDTO>> GetAllCountriesAsync()
+    {
+        return Task.FromResult<IEnumerable<CountryDTO>>(_countries);
+    }
+
+    public Task<CountryDTO?> GetCountryByIDAsync(int countryID)
+    {
+        var country = _countries.FirstOrDefault(c => c.CountryID == countryID);
+        return Task.FromResult(country);
+    }
+
+    public Task<CountryDTO?> GetCountryByNameAsync(string countryName)
+    {
+        var country = _countries.FirstOrDefault(c => c.CountryName.Equals(countryName, StringComparison.OrdinalIgnoreCase));
+        return Task.FromResult(country);
+    }
+}
