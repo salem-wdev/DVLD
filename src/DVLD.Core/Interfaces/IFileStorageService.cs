220ec96d84480ad0ed528959d62b157a9710f7ce
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-11 20:05:35 +0300

    Add IFileStorageService interface for file operations
    A new namespace `DVLD.Core.Interfaces` has been introduced, and the `IFileStorageService` interface has been added. This interface provides methods for file and folder operations, including:
    
    - Creating a folder if it does not exist.
    - Replacing a file name with a GUID.
    - Copying a file to a destination folder with a GUID-based name asynchronously.
    - Deleting a specified file.

---
+++ src/DVLD.Core/Interfaces/IFileStorageService.cs
@@ -0,0 +1,13 @@
+﻿namespace DVLD.Core.Interfaces
+{
+    public interface IFileStorageService
+    {
+        bool CreateFolderIfDoesNotExist(string FolderPath);
+
+        string ReplaceFileNameWithGUID(string sourceFile);
+
+        Task<string> CopyFileToDestinationFolderWithGUIDAsync(string sourceFile, string destinationFolder);
+
+        bool DeleteFile(string sourceFile);
+    }
+}
