d2746a671b06a66f7f08de34fe80059927328623
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-11 19:04:30 +0300

    Add IPersonService interface for person management
    Introduced a new `IPersonService` interface in the `DVLD.Core.Interfaces` namespace to handle person-related operations.
    
    The interface includes methods for adding, updating, deleting, and retrieving persons, as well as checking existence and managing national numbers. It relies on the `Person` entity and `DVLD.Core.Enums`. Added `System.Data` namespace for potential database-related functionality.

---
+++ src/DVLD.Core/Interfaces/IPersonService.cs
@@ -0,0 +1,31 @@
+﻿using DVLD.Core.Entities;
+using DVLD.Core.Enums;
+using System.Data;
+
+namespace DVLD.Core.Interfaces
+{
+    public interface IPersonService
+    {
+        Task<bool> AddNewAsync();
+
+        Task<bool> UpdateAsync();
+
+        Task<bool> DeleteAsync(int? PersonID);
+
+        Task<Person> FindAsync(int? PersonID);
+
+        Task<Person> FindAsync(string NationalNo);
+
+        Task<bool> IsExistsAsync(int? PersonID);
+
+        Task<bool> IsExistsAsync(string NationalNo);
+
+        Task<bool> IsNationalNoUsedAsync(int? PersonID, string NationalNo);
+
+        Task<IEnumerable<Person>> GetAllAsync();
+
+        Task<bool> HasPeopleAsync();
+
+        Task<Person> CreateNewPersonAsync(Person person);
+    }
+}
