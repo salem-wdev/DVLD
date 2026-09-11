5b7cde3fbc35ec0765617de6de5dd5d7fc9b4246
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-11 15:34:37 +0300

    Add IPersonRepository interface for person management
    A new `IPersonRepository` interface has been introduced in the
    `DVLD.Core.Interfaces` namespace to manage person-related
    operations. The interface includes methods for retrieving,
    adding, updating, and deleting person records, as well as
    checking for existence and managing national numbers.
    
    Additionally, `System.Data` and `DVLD.Core.Entities` namespaces
    were added to support `DataTable` usage and the `Person` entity.

---
+++ src/DVLD.Core/Interfaces/IPersonRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using DVLD.Core.Entities;
+
+namespace DVLD.Core.Interfaces
+{
+    public interface IPersonRepository
+    {
+        Task<Person> GetPersonInfoByIDAsync(int? PersonID);
+
+        Task<Person> GetPersonInfoByNationalNoAsync(string NationalNo);
+
+        Task<int> AddNewPersonAsync(Person person);
+
+        Task<bool> UpdatePersonAsync(Person person);
+
+        Task<bool> IsPersonExistsAsync(int? PersonID);
+
+        Task<bool> IsPersonExistsAsync(string NationalNo);
+
+        Task<bool> IsNationalNoUsedAsync(int? PersonID, string NationalNo);
+
+        Task<bool> DeletePersonAsync(int? PersonID);
+
+        Task<DataTable> GetAllPeopleAsync();
+
+        Task<bool> HasPeopleAsync();
+    }
+}
