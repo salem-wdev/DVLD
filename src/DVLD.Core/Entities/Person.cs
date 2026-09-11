1688370fcb8a07634ae01e3d70d28d1043126a81
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-11 15:26:15 +0300

    Add `Person` class to represent individual entities
    A new `Person` class has been introduced in the `DVLD.Core.Entities` namespace. This class includes properties such as `PersonID`, `NationalNo`, `FirstName`, `LastName`, `DateOfBirth`, `Gender`, `Address`, and others to represent an individual's details.
    
    The `FullName` property computes a concatenated name from non-null and non-whitespace components. A constructor has been added to initialize all properties.
    
    Additionally, a `using` directive for `DVLD.Core.Enums` has been added to support the `GenderType` enumeration.

---
+++ src/DVLD.Core/Entities/Person.cs
@@ -0,0 +1,49 @@
+﻿using DVLD.Core.Enums;
+
+namespace DVLD.Core.Entities
+{
+    public class Person
+    {
+        public int? PersonID { get;}
+
+        public string NationalNo { get; }
+        public string FirstName { get;}
+        public string SecondName { get;}
+        public string? ThirdName { get;}
+        public string LastName { get;}
+
+        public string FullName =>
+                string.Join(" ", new[] { FirstName, SecondName, ThirdName, LastName }
+                    .Where(name => !string.IsNullOrWhiteSpace(name)));
+
+        public DateTime DateOfBirth { get;}
+        public GenderType Gender { get;}
+        public string Address { get;}
+        public string Phone { get;}
+        public string? Email { get;}
+        public int NationalityCountryID { get;}
+        public string? ImagePath { get;}
+        public Country? Country { get; }
+
+        public Person(int? personID, string nationalNo, string firstName, string secondName,
+            string? thirdName, string lastName, DateTime dateOfBirth, GenderType gender, string address, string phone,
+            string? email, int nationalityCountryID, string? imagePath, Country? country)
+        {
+            PersonID = personID;
+            NationalNo = nationalNo;
+            FirstName = firstName;
+            SecondName = secondName;
+            ThirdName = thirdName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            Address = address;
+            Phone = phone;
+            Email = email;
+            NationalityCountryID = nationalityCountryID;
+            ImagePath = imagePath;
+            Country = country;
+        }
+
+    }
+}
