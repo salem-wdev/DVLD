3469d4e2de569a8d7b084b9e0b242325ad17f941
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-11 15:34:07 +0300

    Add PersonRequestDTO record to represent person data
    A new `PersonRequestDTO` record was introduced in the `DVLD.Core.DTOs.People` namespace. This record includes properties such as `PersonID`, `NationalNo`, `FirstName`, `LastName`, `DateOfBirth`, `Gender`, and others to encapsulate person-related data.
    
    Additionally, `using` directives for `System`, `System.Collections.Generic`, `System.Linq`, and `DVLD.Core.Enums` were added to support the new record.

---
+++ src/DVLD.Core/DTOs/People/PersonRequestDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DVLD.Core.Enums;
+namespace DVLD.Core.DTOs.People
+{
+     record PersonRequestDTO(
+
+        int? PersonID,
+
+        string NationalNo,
+        string FirstName,
+        string SecondName,
+        string? ThirdName,
+        string LastName,
+
+        DateTime DateOfBirth,
+        GenderType Gender,
+        string Address,
+        string Phone,
+        string? Email,
+        int NationalityCountryID,
+        string? ImagePath
+    )
+    { }
+}
