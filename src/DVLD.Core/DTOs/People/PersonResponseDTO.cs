09122d9e6986f34b558ddd8fc33e3cd4e4245d17
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-11 15:29:25 +0300

    Add PersonResponseDTO record to represent person data
    A new `PersonResponseDTO` record was introduced in the
    `DVLD.Core.DTOs.People` namespace. This record includes
    properties such as `PersonID`, `NationalNo`, `FirstName`,
    `LastName`, `DateOfBirth`, `Gender`, and others to represent
    person-related data.
    
    A computed property `FullName` was added to generate a
    concatenated full name from the available name fields.
    
    The `DVLD.Core.Enums` namespace was imported to use the
    `GenderType` enum.

---
+++ src/DVLD.Core/DTOs/People/PersonResponseDTO.cs
@@ -0,0 +1,28 @@
+﻿using DVLD.Core.Enums;
+
+namespace DVLD.Core.DTOs.People
+{
+    public record PersonResponseDTO(
+
+        int? PersonID,
+
+        string NationalNo,
+        string FirstName,
+        string SecondName,
+        string? ThirdName,
+        string LastName,
+
+        DateTime DateOfBirth,
+        GenderType Gender,
+        string Address,
+        string Phone,
+        string? Email,
+        int NationalityCountryID,
+        string? ImagePath
+    )
+    {
+        public string FullName =>
+               string.Join(" ", new[] { FirstName, SecondName, ThirdName, LastName }
+                   .Where(name => !string.IsNullOrWhiteSpace(name)));
+    }
+}
