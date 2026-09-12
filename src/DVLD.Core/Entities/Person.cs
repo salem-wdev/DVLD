d12545b0159487aae56c0c1706d36fade4d772b7
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-12 16:22:52 +0300

    Refactor Person class and add validation methods
    Refactored the `Person` class to improve encapsulation and validation:
    - Updated `using` directives to include `DVLD.Core.Common`.
    - Changed the constructor from `public` to `protected` and removed the `PersonID` parameter.
    - Added `_IsValidInfo` method to validate input fields for creating a `Person` object.
    - Introduced a `Create` factory method to handle object creation with validation.

--- src/DVLD.Core/Entities/Person.cs
+++ src/DVLD.Core/Entities/Person.cs
@@ -1,4 +1,6 @@
-﻿using DVLD.Core.Enums;
+﻿using DVLD.Core.Common;
+using DVLD.Core.Enums;
+
 
 namespace DVLD.Core.Entities
 {
@@ -25,7 +27,7 @@ namespace DVLD.Core.Entities
         public string? ImagePath { get;}
         public Country? Country { get; }
 
-        public Person(int? personID, string nationalNo, string firstName, string secondName,
+        protected Person(string nationalNo, string firstName, string secondName,
             string? thirdName, string lastName, DateTime dateOfBirth, GenderType gender, string address, string phone,
             string? email, int nationalityCountryID, string? imagePath, Country? country)
         {
@@ -43,6 +45,58 @@ namespace DVLD.Core.Entities
             NationalityCountryID = nationalityCountryID;
             ImagePath = imagePath;
             Country = country;
+        }
+
+        private static Result<bool> _IsValidInfo(string NationalNo, string FirstName, string SecondName,
+             string LastName, DateTime DateOfBirth, string Address,
+            string Phone, int NationalityCountryID, string? Email)
+        {
+            var fields = new (string Name, string Value)[]
+            {
+                (nameof(NationalNo), NationalNo),
+                (nameof(FirstName), FirstName),
+                (nameof(SecondName), SecondName),
+                (nameof(LastName), LastName),
+                (nameof(Address), Address),
+                (nameof(Phone), Phone)
+            };
+
+            var emptyField = fields.FirstOrDefault(f => string.IsNullOrWhiteSpace(f.Value));
+
+            if (emptyField.Name != null)
+            {
+                return Result<bool>.Failure($"The field '{emptyField.Name}' is required and cannot be empty.");
+            }
+
+            if (NationalityCountryID < 1)
+            {
+                return Result<bool>.Failure("Invalid nationality country ID.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(Email) && !Email.Contains("@"))
+            {
+                return Result<bool>.Failure("Invalid email format.");
+            }
+
+
+
+            return Result<bool>.Success(true);
+        }
+       
+        public static Result<Person?> Create(string nationalNo, string firstName, string secondName,
+            string? thirdName, string lastName, DateTime dateOfBirth, GenderType gender, string address, string phone,
+            string? email, int nationalityCountryID, string? imagePath, Country? country)
+        {
+
+            var validationResult = _IsValidInfo(nationalNo, firstName, secondName, lastName, dateOfBirth
+                , address, phone, nationalityCountryID, email);
+            if (validationResult.IsFailure)
+            {
+                return Result<Person?>.Failure("Invalid person information.");
+            }
+
+            return Result<Person?>.Success(new Person(nationalNo, firstName, secondName, thirdName, lastName, dateOfBirth
+                , gender, address, phone, email, nationalityCountryID, imagePath, country));
         }
 
     }
