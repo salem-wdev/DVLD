a9a2e406ee35d6e3ec074200b3bd82bdcc23052e
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-14 10:47:02 +0300

    Refactor IFileStorageService interface
    Updated the namespace to include `DVLD.Core.Common`. Removed the `ReplaceFileNameWithGUID` method. Changed the return type of `CopyFileToDestinationFolderWithGUIDAsync` to `Task<Result<string>>`. Added a new method `IsFileExists` to enhance file existence checks.

--- src/DVLD.Core/Interfaces/IFileStorageService.cs
+++ src/DVLD.Core/Interfaces/IFileStorageService.cs
@@ -1,13 +1,12 @@
-﻿namespace DVLD.Core.Interfaces
+﻿using DVLD.Core.Common;
+namespace DVLD.Core.Interfaces
 {
     public interface IFileStorageService
     {
         bool CreateFolderIfDoesNotExist(string FolderPath);
-
         string ReplaceFileNameWithGUID(string sourceFile);
-
-        Task<string> CopyFileToDestinationFolderWithGUIDAsync(string sourceFile, string destinationFolder);
-
+        Task<Result<string>> CopyFileToDestinationFolderWithGUIDAsync(string sourceFile, string destinationFolder);
         bool DeleteFile(string sourceFile);
+        bool IsFileExists(string sourceFile);
     }
 }
