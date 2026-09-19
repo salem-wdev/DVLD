0e724b70fb766ab91586075e961141267d0f4e45
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-19 14:43:34 +0300

    Add IEventAggregator interface for domain event handling
    Introduced a new `IEventAggregator` interface in the
    `DVLD.Application.Interfaces.Events` namespace. This interface
    includes two asynchronous methods: `PublishBeforeCommitAsync`
    and `PublishAfterCommitAsync`, designed to handle domain event
    publishing before and after commit operations, respectively.
    
    Added a `using` directive for `DVLD.Domain.Common` to support
    the use of `IDomainEvent` in the interface.

---
+++ src/DVLD.Application/Interfaces/Events/IEventAggregator.cs
@@ -0,0 +1,9 @@
+﻿using DVLD.Domain.Common;
+
+namespace DVLD.Application.Interfaces.Events;
+
+public interface IEventAggregator
+{
+    Task PublishBeforeCommitAsync(IEnumerable<IDomainEvent> domainEvents);
+    Task PublishAfterCommitAsync(IEnumerable<IDomainEvent> domainEvents);
+}
