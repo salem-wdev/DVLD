0a56fecf5a9ed8bc31582c6acdb65bf617a08fd9
Salem A Habtor <salem.habtor.dev@gmail.com>
2026-09-19 14:37:11 +0300

    Add IBeforeCommitHandler interface for pre-commit events
    A new interface `IBeforeCommitHandler<TEvent>` was introduced in the `DVLD.Application.Interfaces.Events` namespace. This interface inherits from `IEventHandler` and is constrained to types implementing `IDomainEvent`. It defines an asynchronous `HandleAsync` method for handling events of type `TEvent`. Additionally, a `using` directive for `DVLD.Domain.Common` was added to support the new interface.

---
+++ src/DVLD.Application/Interfaces/Events/IBeforeCommitHandler.cs
@@ -0,0 +1,9 @@
+﻿using DVLD.Domain.Common;
+
+namespace DVLD.Application.Interfaces.Events;
+
+public interface IBeforeCommitHandler<in TEvent> : IEventHandler
+    where TEvent : IDomainEvent
+{
+    Task HandleAsync(TEvent @event);
+}
